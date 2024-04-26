3435a5119fe190d476db3d8e41736526eba1de17
devesh237 <91526568+devesh237@users.noreply.github.com>
2024-04-26 17:58:21 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -73,11 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/Alllllllllllllllllllllll"
+                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://"t.me/shanutrp98"
             ),
             InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/+f9ZegEtS4U4wNDdl"
-            ),
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/+6HQXFJ7jqbdkNzg9/),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
